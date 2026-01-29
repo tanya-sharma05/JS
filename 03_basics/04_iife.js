4f9780582e50a76f0f8481f9623963e5bd5532fb
Tanya <tanyasharma7658@gmail.com>
2026-01-29 11:05:25 +0530

    Enhance IIFE documentation with comments and examples
    Added comments explaining Immediately Invoked Function Expressions (IIFE) and provided examples.

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,7 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+// Syntax ()()
+// IIFE is a function which immediately executes after it's creation. 
+// It is written as one parentheses for wrapping the function and other for execution. 
 
 (function chai(){
     // named IIFE
@@ -8,5 +10,4 @@
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
-
+} )('hitesh') // can pass argument in second parentheses 
