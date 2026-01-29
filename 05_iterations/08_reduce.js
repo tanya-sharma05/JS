e360e7b2ccd0491f401dd411ff05011123b6f86c
Tanya <tanyasharma7658@gmail.com>
2026-01-29 13:32:28 +0530

    Refactor reduce function in 08_reduce.js
    Refactor reduce function to use arrow syntax and remove commented code.

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -3,12 +3,10 @@ const myNums = [1, 2, 3]
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
-// }, 0)
+// }, 0) // provide a value from where to start calculation
 
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
-
 console.log(myTotal);
-
 
 const shoppingCart = [
     {
