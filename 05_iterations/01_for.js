ac088341d135558ae5bf9ba55228d37dd8f61ab3
Tanya <tanyasharma7658@gmail.com>
2026-01-29 11:18:43 +0530

    Refactor loop logging and control statements
    Refactor console.log statements and adjust loop structures for clarity.

--- 05_iterations/01_for.js
+++ 05_iterations/01_for.js
@@ -5,8 +5,7 @@ for (let i = 0; i <= 10; i++) {
     if (element == 5) {
         //console.log("5 is best number");
     }
-    //console.log(element);
-    
+    //console.log(element); 
 }
 
 // console.log(element);
@@ -16,34 +15,31 @@ for (let i = 1; i <= 10; i++) {
    for (let j = 1; j <= 10; j++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
     //console.log(i + '*' + j + ' = ' + i*j );
-   }
-    
+   }    
 }
+
 let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
     //console.log(element);
-    
 }
 
-
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // break and continue
-
-// for (let index = 1; index <= 20; index++) {
-//     if (index == 5) {
-//         console.log(`Detected 5`);
-//         break
-//     }
-//    console.log(`Value of i is ${index}`);
-    
-// }
 
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log(`Detected 5`);
-        continue
+        break
     }
    console.log(`Value of i is ${index}`);
-    
+}
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue // skip value
+    }
+   console.log(`Value of i is ${index}`);
 }
