415d44b099ea351e0150138155f0340686283146
Tanya <tanyasharma7658@gmail.com>
2026-01-29 13:17:56 +0530

    Enhance for...of loop example with comments
    Added comments explaining the use of for...of loop with arrays, strings, maps, and objects.

--- 05_iterations/03_for_of.js
+++ 05_iterations/03_for_of.js
@@ -1,27 +1,28 @@
 // for of
 
+// for of loop works on arrays, strings and maps to iterate and print their values.
 // ["", "", ""]
 // [{}, {}, {}]
 
+// Arrays 
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
     //console.log(num);
 }
 
+// Strings
 const greetings = "Hello world!"
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
 
 // Maps
-
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
 map.set('IN', "India")
-
 
 // console.log(map);
 
@@ -29,12 +30,12 @@ for (const [key, value] of map) {
     // console.log(key, ':-', value);
 }
 
+// Objects
 const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
+// for (const [key, value] of myObject) { // does not work on objects
+//     console.log(key, ':-', value);    
 // }
