9c417c02f90d1610b52bb875050fa16b9f1fc10a
Tanya <tanyasharma7658@gmail.com>
2026-01-30 13:44:26 +0530

    Refactor promise handling and error management

--- 09_advance_concepts/02_promise.js
+++ 09_advance_concepts/02_promise.js
@@ -1,6 +1,7 @@
+// For async task like DB calls, cryptography, network
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 const promiseOne = new Promise(function(resolve, reject){
-    // Do an async task
-    // DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is compelete');
         resolve()
@@ -11,16 +12,17 @@ promiseOne.then(function(){
     console.log("Promise consumed");
 })
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Async task 2");
         resolve()
     }, 1000)
-
 }).then(function(){
     console.log("Async 2 resolved");
 })
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
         resolve({username: "Chai", email: "chai@example.com"})
@@ -31,18 +33,20 @@ promiseThree.then(function(user){
     console.log(user);
 })
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
         let error = true
         if (!error) {
             resolve({username: "hitesh", password: "123"})
-        } else {
+        } 
+        else {
             reject('ERROR: Something went wrong')
         }
     }, 1000)
 })
 
- promiseFour
+promiseFour
  .then((user) => {
     console.log(user);
     return user.username
@@ -52,14 +56,15 @@ const promiseFour = new Promise(function(resolve, reject){
     console.log(error);
 }).finally(() => console.log("The promise is either resolved or rejected"))
 
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Asyn await 
 const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
         let error = true
         if (!error) {
             resolve({username: "javascript", password: "123"})
-        } else {
+        } 
+        else {
             reject('ERROR: JS went wrong')
         }
     }, 1000)
@@ -69,26 +74,29 @@ async function consumePromiseFive(){
     try {
         const response = await promiseFive
         console.log(response);
-    } catch (error) {
+    } 
+    catch (error) {
         console.log(error);
     }
 }
 
 consumePromiseFive()
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 // async function getAllUsers(){
 //     try {
 //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
 //         const data = await response.json()
 //         console.log(data);
-//     } catch (error) {
+//     }
+//     catch (error) {
 //         console.log("E: ", error);
 //     }
 // }
 
 //getAllUsers()
 
+// Using fetch directly 
 fetch('https://api.github.com/users/hiteshchoudhary')
 .then((response) => {
     return response.json()
